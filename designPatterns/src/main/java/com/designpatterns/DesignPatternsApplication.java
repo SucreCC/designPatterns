db595ec443fc93b3e310e5e1d628aa536f03a2c8
dengkai <dengkai@wxchina.com>
2021-09-03 16:53:04 +0800

    add Strategy Mode

--- designPatterns/src/main/java/com/designpatterns/DesignPatternsApplication.java
+++ designPatterns/src/main/java/com/designpatterns/DesignPatternsApplication.java
@@ -2,14 +2,13 @@ package com.designpatterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动类
  * @author dengkai
  * @date 2021/9/3 9:35
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class DesignPatternsApplication {
 
     public static void main(String[] args) {
